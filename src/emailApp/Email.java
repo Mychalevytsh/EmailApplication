c60220ec5de287a58598f3da405fb86259cf29de
Ivan Mykhalevych <mychalevytsh@gmail.com>
2018-12-19 17:47:25 +0200

    create class Email

---
+++ src/emailApp/Email.java
@@ -0,0 +1,28 @@
+package emailApp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailCapacity;
+    private String alternateEmail;
+
+    //Constructors: firstName and LastName
+
+    public Email( String firstName, String lastName ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("We create Email for:"+ this.firstName +" "+this.lastName);
+    }
+
+    //Ask of department
+
+    //Generate random password
+
+    //Set capacity of EmailBox
+
+    //Set alternateEmail
+
+
+}
